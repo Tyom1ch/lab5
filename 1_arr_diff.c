d33928edc1fba424fe90eb46e480386081931301
Tyom1ch <tyomchbb@gmail.com>
2025-04-28 18:30:43 +0300

    лолкєк

--- 1_arr_diff.c
+++ 1_arr_diff.c
@@ -1,39 +1,55 @@
-// file: array_diff.c
 #include <stdio.h>
 
 #define MAX_SIZE 100
 
 int main() {
-    int a[MAX_SIZE], b[MAX_SIZE], c[MAX_SIZE];
-    int n, i;
+    int choice;
 
-    printf("Введіть кількість елементів масивів: ");
-    scanf("%d", &n);
+    while (1) {
+        printf("\nОберіть дію:\n");
+        printf("1. Обчислити різницю масивів (c[i] = a[i] - b[i])\n");
+        printf("0. Вихід\n");
+        printf("Ваш вибір: ");
+        scanf("%d", &choice);
 
-    if (n > MAX_SIZE) {
-        printf("Перевищено максимальний розмір масиву (%d)\n", MAX_SIZE);
-        return 1;
-    }
+        if (choice == 0) {
+            printf("Завершення програми.\n");
+            break; // Вихід з циклу while
+        } else if (choice == 1) {
+            int a[MAX_SIZE], b[MAX_SIZE], c[MAX_SIZE];
+            int n, i;
 
-    printf("Введіть елементи масиву a:\n");
-    for (i = 0; i < n; i++) {
-        printf("a[%d] = ", i);
-        scanf("%d", &a[i]);
-    }
+            printf("Введіть кількість елементів масивів: ");
+            scanf("%d", &n);
 
-    printf("Введіть елементи масиву b:\n");
-    for (i = 0; i < n; i++) {
-        printf("b[%d] = ", i);
-        scanf("%d", &b[i]);
-    }
+            if (n > MAX_SIZE) {
+                printf("Перевищено максимальний розмір масиву (%d)\n", MAX_SIZE);
+                continue; // Повернення до початку циклу while
+            }
 
-    for (i = 0; i < n; i++) {
-        c[i] = a[i] - b[i];
-    }
+            printf("Введіть елементи масиву a:\n");
+            for (i = 0; i < n; i++) {
+                printf("a[%d] = ", i);
+                scanf("%d", &a[i]);
+            }
 
-    printf("Масив c (a[i] - b[i]):\n");
-    for (i = 0; i < n; i++) {
-        printf("c[%d] = %d\n", i, c[i]);
+            printf("Введіть елементи масиву b:\n");
+            for (i = 0; i < n; i++) {
+                printf("b[%d] = ", i);
+                scanf("%d", &b[i]);
+            }
+
+            for (i = 0; i < n; i++) {
+                c[i] = a[i] - b[i];
+            }
+
+            printf("Масив c (a[i] - b[i]):\n");
+            for (i = 0; i < n; i++) {
+                printf("c[%d] = %d\n", i, c[i]);
+            }
+        } else {
+            printf("Невірний вибір. Спробуйте ще раз.\n");
+        }
     }
 
     return 0;
--- 2_diff_keyb_a.c
+++ 2_diff_keyb_a.c
@@ -1,56 +1,89 @@
-// file: array_manual.c
 #include <stdio.h>
-
-#define MAX_SIZE 100
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int arr[MAX_SIZE];
-    int n, i;
-    int max, firstPos = -1, secondPos = -1, sum = 0;
+    int choice;
 
-    printf("Введіть кількість елементів масиву: ");
-    scanf("%d", &n);
+    srand(time(NULL)); // Ініціалізація генератора випадкових чисел
 
-    if (n > MAX_SIZE) {
-        printf("Максимально дозволено %d елементів.\n", MAX_SIZE);
-        return 1;
-    }
+    while (1) {
+        printf("\nОберіть завдання:\n");
+        printf("1. Ввести масив з клавіатури та виконати завдання.\n");
+        printf("2. Згенерувати масив випадковими числами та виконати завдання.\n");
+        printf("0. Вихід\n");
+        printf("Ваш вибір: ");
+        scanf("%d", &choice);
 
-    printf("Введіть елементи масиву:\n");
-    for (i = 0; i < n; i++) {
-        printf("arr[%d] = ", i);
-        scanf("%d", &arr[i]);
-    }
+        if (choice == 0) {
+            printf("Завершення програми.\n");
+            break;
+        } else if (choice == 1 || choice == 2) {
+            int n;
 
-    // Пошук максимального елемента
-    max = arr[0];
-    for (i = 1; i < n; i++) {
-        if (arr[i] > max) max = arr[i];
-    }
+            printf("Введіть розмірність масиву: ");
+            scanf("%d", &n);
 
-    // Пошук першого і другого додатного елементу
-    for (i = 0; i < n; i++) {
-        if (arr[i] > 0) {
-            if (firstPos == -1)
-                firstPos = i;
-            else {
-                secondPos = i;
-                break;
+            if (n <= 0) {
+                printf("Розмірність масиву повинна бути більше нуля.\n");
+                continue;
             }
+
+            int arr[n];
+
+            if (choice == 1) {
+                printf("Введіть елементи масиву:\n");
+                for (int i = 0; i < n; i++) {
+                    printf("arr[%d]: ", i);
+                    scanf("%d", &arr[i]);
+                }
+            } else {
+                printf("Згенеровано масив:\n");
+                for (int i = 0; i < n; i++) {
+                    arr[i] = (rand() % 201) - 100; // Генерація випадкових чисел від -100 до 100
+                    printf("%d ", arr[i]);
+                }
+                printf("\n");
+            }
+
+            // 1) Знайти максимальний елемент
+            int max_element = arr[0];
+            for (int i = 1; i < n; i++) {
+                if (arr[i] > max_element) {
+                    max_element = arr[i];
+                }
+            }
+            printf("Максимальний елемент масиву: %d\n", max_element);
+
+            // 2) Сума елементів між першим та другим додатними елементами
+            int first_positive_index = -1;
+            int second_positive_index = -1;
+            int sum_between = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (arr[i] > 0) {
+                    if (first_positive_index == -1) {
+                        first_positive_index = i;
+                    } else if (second_positive_index == -1) {
+                        second_positive_index = i;
+                        break; // Знайшли другий додатний, далі не шукаємо
+                    }
+                }
+            }
+
+            if (first_positive_index != -1 && second_positive_index != -1) {
+                for (int i = first_positive_index + 1; i < second_positive_index; i++) {
+                    sum_between += arr[i];
+                }
+                printf("Сума елементів між першим і другим додатними елементами: %d\n", sum_between);
+            } else {
+                printf("В масиві менше двох додатних елементів.\n");
+            }
+
+        } else {
+            printf("Невірний вибір. Спробуйте ще раз.\n");
         }
     }
-
-    if (firstPos != -1 && secondPos != -1) {
-        for (i = firstPos + 1; i < secondPos; i++) {
-            sum += arr[i];
-        }
-    } else {
-        printf("У масиві менше двох додатних елементів.\n");
-    }
-
-    printf("Максимальний елемент: %d\n", max);
-    if (secondPos != -1)
-        printf("Сума між першим і другим додатним елементом: %d\n", sum);
 
     return 0;
 }
